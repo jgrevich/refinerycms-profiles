8466a32036ccd9f05628d37636f6a61689d1b47d
Justin Grevich <jgrevich@gmail.com>
2012-09-06 14:48:59 -0700

    fixed error in profiles_mailer from the merge.

--- app/mailers/refinery/profiles/profile_mailer.rb
+++ app/mailers/refinery/profiles/profile_mailer.rb
@@ -1,12 +1,13 @@
 module Refinery
   module Profiles
     class ProfileMailer < ActionMailer::Base
-  default :from => "iem@ucsd.edu"
+      default :from => "iem@ucsd.edu"
   
-  def email_token(profile)
-    @profile = profile
-    @url = [profile_url(@profile, :host => "test.iem.ucsd.edu"), profile.token].join('/')
-    mail(:to => profile.email, :subject => "iem.ucsd.edu Profile Update")
+      def email_token(profile)
+        @profile = profile
+        @url = [refinery.profiles_profile_url(@profile, :host => "test.iem.ucsd.edu"), profile.token].join('/')
+        mail(:to => profile.email, :subject => "iem.ucsd.edu Profile Update")
+      end
+    end
   end
-  
 end
